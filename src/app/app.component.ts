314e853872da08384c34b6a5e83284ec3f8db99c
marianagsoares <marianasoares.ti@gmail.com>
2022-11-05 15:34:25 -0300

    5 - Submissão do formulário

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'buscar-cep';
+
+  // class ForBiulder
+    //metodo array
+    //metodo control
+    //metodo group
+    //metodo record
+
+  
+  constructor(private formBuilder: FormBuilder) {}
+
+  formulario: FormGroup = this.formBuilder.group({
+    inputCep: ["", [Validators.required]]
+  })
+
+  
+  procurar() {
+  
+    const valorInputCep = this.formulario.get('inputCep')?.value
+    console.log(valorInputCep)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MaterialModule } from './material/material.module';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
