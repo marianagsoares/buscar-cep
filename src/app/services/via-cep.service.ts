1def70c872e9d88e46ca2d06456747fff28c1446
marianagsoares <marianasoares.ti@gmail.com>
2022-11-05 16:22:31 -0300

    6 - Consumir API do ViaCep

--- src/app/services/via-cep.service.ts
+++ src/app/services/via-cep.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ICep } from '../interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class ViaCepService {
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  private readonly baseUrl: string = "https://viacep.com.br/ws"
+
+  buscarCep(cep: string) {
+    return this.http.get<ICep>(`${this.baseUrl}/${cep}/json`)
+  }
 }
