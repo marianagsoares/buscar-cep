f055e32fd591b14a06a6ee44b4f5645c227449c0
marianagsoares <marianasoares.ti@gmail.com>
2022-11-05 13:54:50 -0300

    3 - Instalar e configurar biblioteca do angular material

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core'
+
+//material
+import { MatSnackBarModule } from '@angular/material/snack-bar'
+
+@NgModule({
+  exports: [
+    MatSnackBarModule
+  ]
+})
+
+export class MaterialModule {}
